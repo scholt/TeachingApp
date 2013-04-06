dc1778278fc93b98bbdac3649947044b67bfea4c
Uwe Kamper <me@uwekamper.de>
2013-04-06 16:58:36 +0200

    DummyContent entfernt

--- src/de/tobiasfiebiger/mobile/teachapp/MaterialGridFragment.java
+++ src/de/tobiasfiebiger/mobile/teachapp/MaterialGridFragment.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 import android.widget.TextView;
-import de.tobiasfiebiger.mobile.teachapp.dummy.DummyContent;
 import de.tobiasfiebiger.mobile.teachapp.model.Material;
 import de.tobiasfiebiger.mobile.teachapp.widget.MaterialAdapter;
 
