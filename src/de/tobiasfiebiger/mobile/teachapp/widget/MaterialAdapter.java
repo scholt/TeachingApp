48896a1125e282aafc17ec0303039a0ce8bcb8b0
Uwe Kamper <me@uwekamper.de>
2013-04-06 16:07:48 +0200

    MaterialAdapter compiled aber man sieht nix

--- src/de/tobiasfiebiger/mobile/teachapp/widget/MaterialAdapter.java
+++ src/de/tobiasfiebiger/mobile/teachapp/widget/MaterialAdapter.java
@@ -1,13 +1,26 @@
 package de.tobiasfiebiger.mobile.teachapp.widget;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
 import de.tobiasfiebiger.mobile.teachapp.R;
+import de.tobiasfiebiger.mobile.teachapp.TeachingApp;
 import de.tobiasfiebiger.mobile.teachapp.model.Material;
+import de.tobiasfiebiger.mobile.teachapp.util.ImageCache;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 public class MaterialAdapter extends MasterAdapter<Material> {
+	
+	private static final int IO_BUFFER_SIZE = 4 * 1024;
+	private static final String TAG = "MaterialAdapter";
 
 	public MaterialAdapter(Activity context) {
 		super(context);
@@ -19,18 +32,58 @@ public class MaterialAdapter extends MasterAdapter<Material> {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
+        final ImageView imageView;
         if (convertView == null) {
         	convertView = inflater.inflate(R.layout.material_gridview_item, null);
             imageView = (ImageView) convertView.findViewById(R.id.material_thumbnail);
         } else {
             imageView = (ImageView) convertView;
         }
+        String url = "http://uwekamper.de/media/img/avatar_500.jpg";
+        
+        TeachingApp.getImageCache().requestImage(url, new ImageCache.BitmapCallback() {
+			
+			@Override
+			public void onCacheError(Exception error) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onBitmapLoaded(Bitmap image) {
+				// TODO Auto-generated method stub
+				imageView.setImageBitmap(image);
+			}
+		});
 
-        imageView.setImageResource(mThumbIds[position]);
         return imageView;
     }
 	
+	private void closeStream(BufferedInputStream str) {
+		if (str == null)
+			return;
+		
+		try {
+			str.close();
+		} 
+		catch (Exception e) {
+			Log.e(TAG, "Error closing stream.");
+		}
+	}
+	
+	private void closeStream(BufferedOutputStream str) {
+		
+		if (str == null)
+			return;
+		
+		try {
+			str.close();
+		} 
+		catch (Exception e) {
+			Log.e(TAG, "Error closing stream.");
+		}
+	}
+
 	// references to our images
     private Integer[] mThumbIds = {
     		R.drawable.ic_launcher
